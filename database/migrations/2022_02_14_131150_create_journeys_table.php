d1553c424f532db1ac5f08bf1a7e009cd86d103a
SebastianCarai <sebastiancarai7@gmail.com>
2022-02-14 15:08:33 +0100

    first commit

---
+++ database/migrations/2022_02_14_131150_create_journeys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJourneysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journeys', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 30);
+            $table->decimal('price',8,2);
+            $table->date('start_journey_date');
+            $table->date('end_journey_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journeys');
+    }
+}
---
+++ database/seeds/JourneysTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Journey;
+
+class JourneysTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<10; $i++){
+            $new_journey = new Journey();
+            $new_journey->destination = $faker->state;
+            $new_journey->price = $faker->randomFloat(2, 100, 10000);
+            $new_journey->start_journey_date = $faker->dateTime;
+            $new_journey->end_journey_date = $faker->dateTime;
+            $new_journey->destination_description = $faker->paragraphs();
+            $new_journey->save();
+        }
+    }   
+}
